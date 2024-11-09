7f200b6767d712e510321d5a52fbb17a2abf0dd8
Luis Adames <adamesluis2024@outlook.com>
2024-11-09 17:25:57 -0400

    ok

---
+++ CSB/CalcularSalario/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CalcularSalario();
+    }
+
+    /// <summary>
+    /// Calcular el salario de un empleado
+    /// </summary>
+
+    private static void CalcularSalario()
+    {
+        // Declarar sus variables de entrada // 
+        int num1 = 0;
+        int num2 = 0;
+        int suma = 0;
+        int producto = 0;
+
+        //pedir numeros por teclado//
+        Console.WriteLine("Ingrese el vlaor de Num 1: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el valor de Num 2");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Calcular la suma y el producto.
+        suma = (num1 + num2);
+        producto = (num1 * num2);
+
+        // Imprimir valores.
+
+        Console.WriteLine($"La suma es: {suma} y el produucto es: {producto}");
+    }
+
+}
---
+++ promedio/promedio/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Declarar variables
+        int cantidadNotas = 0;
+        int sumaNotas = 0;
+        int nota = 0;
+        double promedio = 0;
+
+        // Pedir la cantidad de notas
+        Console.WriteLine("¿Cuántas notas deseas ingresar?");
+        cantidadNotas = int.Parse(Console.ReadLine());
+
+        // Bucle para pedir cada nota
+        for (int i = 1; i <= cantidadNotas; i++)
+        {
+            Console.WriteLine("Introduce la nota #" + i);
+            nota = int.Parse(Console.ReadLine());
+            sumaNotas += nota;  // Sumar las notas ingresadas
+        }
+
+        // Calcular promedio
+        promedio = (double)sumaNotas / cantidadNotas;
+
+        // Determinar el mensaje a mostrar según el promedio
+        if (promedio >= 7)
+        {
+            Console.WriteLine("Promovido");
+        }
+        else if (promedio >= 4 && promedio < 7)
+        {
+            Console.WriteLine("Regular");
+        }
+        else
+        {
+            Console.WriteLine("Reprobado");
+        }
+
+        // Mostrar el promedio
+        Console.WriteLine("El promedio es: " + promedio);
+    }
+}
